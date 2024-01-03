0517d121430fc5d63b0d48633df99e8ee8bd5987
saultab <vittoriotabare@gmail.com>
2024-01-03 17:11:34 +0100

    various comments

--- simulation/runSimulation.py
+++ simulation/runSimulation.py
@@ -4,6 +4,7 @@ from outputInterface import computePerformance
 from inputInterface import imgToSpikeTrain
 
 import os
+
 # Time step duration in milliseconds
 dt = 0.1
 simulation = "./simulation"
@@ -17,7 +18,7 @@ computationSteps = int(trainDuration / dt)
 inputIntensity = 2.
 
 # Number of images after which the accuracy is evaluated
-updateInterval = 51
+updateInterval = 50
 
 # Network shape
 N_layers = 1
@@ -28,76 +29,74 @@ N_inputs = 784
 rng = np.random.default_rng()
 
 # Mnist test dataset
-images = simulation+"/mnist/t10k-images-idx3-ubyte"
-labels = simulation+"/mnist/t10k-labels-idx1-ubyte"
+images = simulation + "/mnist/t10k-images-idx3-ubyte"
+labels = simulation + "/mnist/t10k-labels-idx1-ubyte"
 
 # File containing the label associated to each neuron in the output layer
-assignmentsFile = simulation+"/networkParameters/assignments.npy"
+assignmentsFile = simulation + "/networkParameters/assignments.npy"
 
 outputCountersFilename = "outputCounters.txt"
-
 
 # Initialize history of spikes
 countersEvolution = np.zeros((updateInterval, N_neurons[-1]))
 
-def write_input_spikes(): 
 
+def write_input_spikes():
     # Import dataset
     imgArray, _ = loadDataset(images, labels)
 
     with open("./inputSpikes.txt", 'w') as filePointer:
 
-        for i in range(updateInterval+1): 
-            print(f"Iteration {i}"); 
+        for i in range(updateInterval + 1):
+            print(f"Iteration {i}");
             spikesTrains = imgToSpikeTrain(imgArray[i], dt, computationSteps, inputIntensity, rng)
             for step in spikesTrains:
                 filePointer.write(str(list(step.astype(int)))
-                                [1:-1].replace(",", "").replace(" ", ""))
+                                  [1:-1].replace(",", "").replace(" ", ""))
                 filePointer.write("\n")
 
-#write_input_spikes()
 
+def compure_accuracy():
+    directory_path = simulation + '/configurations'
 
-def compure_accuracy(): 
-    
-    directory_path = simulation+'/configurations'
-
-# Ottieni il percorso assoluto della cartella
+    # Ottieni il percorso assoluto della cartella
 
     directory_contents = os.listdir(directory_path)
-    accuracies =[]
+    accuracies = []
     outputCounters = np.zeros(N_neurons[-1]).astype(int)
-     
-        # Load the assignments from file
-    with open(assignmentsFile, 'rb') as fp:
-            assignments = np.load(fp)  # len = 400
 
-        # Import dataset
+    # Load the assignments from file
+    with open(assignmentsFile, 'rb') as fp:
+        assignments = np.load(fp)  # len = 400
+
+    # Import dataset
     _, labelsArray = loadDataset(images, labels)
-    
+
     for output in directory_contents:
-    
-        k = 0 
-        i = 0 
+
+        k = 0
+        i = 0
         j = 0
-                
-        with open(directory_path+"/"+ output, "r") as filePointer:
+
+        with open(directory_path + "/" + output, "r") as filePointer:
             for line in filePointer:
-                
+
                 outputCounters[k] = int(line)
                 j += 1
-                k = j%400
+                k = j % 400
 
-                if k==0: 
+                if k == 0:
                     countersEvolution[i % updateInterval] = outputCounters
 
-                    i+=1 
+                    i += 1
 
-                    accuracies = computePerformance(i, updateInterval, countersEvolution, labelsArray, assignments, accuracies)
-        
-        print(output.replace("_",",").replace(".txt","") +"," + accuracies)
+                    accuracies = computePerformance(i, updateInterval, countersEvolution, labelsArray, assignments,
+                                                    accuracies)
 
-        with open(simulation+'/logs/log.txt', 'a') as file:
-            file.write(output.replace("_",",").replace(".txt","") +"," + accuracies+"\n")
+        print(output.replace("_", ",").replace(".txt", "") + "," + accuracies)
 
-compure_accuracy()            
+        with open(simulation + '/logs/log.txt', 'a') as file:
+            file.write(output.replace("_", ",").replace(".txt", "") + "," + accuracies + "\n")
+
+
+compure_accuracy()
--- src/main.rs
+++ src/main.rs
@@ -14,92 +14,66 @@ const N_NEURONS: usize = 400;
 const N_INPUTS: usize = 784;
 const N_INSTANTS: usize = 3500;
 
-fn get_current_dir()->String{
-    let current_dir = std::env::current_dir().expect("Failed to get current directory");
-
-    // Convert the PathBuf to a string
-    let current_dir_str = current_dir.to_string_lossy();
-
-    // Replace backslashes with forward slashes
-    let path = current_dir_str.replace("\\", "/");
-
-    path 
-}
-
-
-fn read_zip(path:&String) -> std::io::Result<()> {
-    // Specify the path to your zip file
-    let extract_to_dir = format!("{path}/simulation/");
-
-    let zip_file_path = format!("{extract_to_dir}inputSpikes.zip");
-
-    // Specify the directory where you want to extract the contents
-
-    // Open the zip file
-    let file = File::open(zip_file_path)?;
-    let mut archive = ZipArchive::new(file)?;
-
-    // Iterate over each file in the zip archive
-    let mut file = archive.by_index(0)?;
-
-    // Construct the output path by appending the extracted file's name to the extraction directory
-    let output_path = format!("{extract_to_dir}inputSpikes.txt"); 
-
-    let mut outfile = File::create(&output_path)?;
-    std::io::copy(&mut file, &mut outfile)?;
-
-    Ok(())
-}
-
-fn main(){
+fn main() {
     let path = get_current_dir();
-    let result = read_zip(&path); 
+
+    /* function that extract the inputSpikes.txt from zip */
+    let result = read_zip(&path);
 
     match result {
-        Ok(zip) => {start_snn();} 
-        Err(e)=>{println!("{:?}",e);}
+        Ok(_zip) => { start_snn(); }
+        Err(e) => { println!("{:?}", e); }
     }
-
 }
 
-fn start_snn(){
-    let vec_comp = vec![Components::VTh, Components::VMem, Components::VReset, Components::VRest, Components::Tau, Components::Ts, Components::Dt, Components::Weights, Components::IntraWeights, Components::PrevSpikes];
-    
+fn start_snn() {
     let path = get_current_dir();
-    /*for elem in vec_comp {
-        for _ in 0..5 {
 
+    /* list of components to simulate */
+    let vec_comp = vec![
+        Components::VTh, Components::VMem,
+        Components::VReset, Components::VRest,
+        Components::Tau, Components::Ts,
+        Components::Dt, Components::Weights,
+        Components::IntraWeights, Components::PrevSpikes];
+
+    /* for each component simulate 5 times accuracy over 50 inputSpikes*/
+    for elem in vec_comp {
+        for _ in 0..5 {
+            /* compute random bit and random index to set fault in precise way */
             let mut rng1 = thread_rng();
             let random_bit = rng1.gen_range(0..64);
-
             let mut rng2 = thread_rng();
             let random_index = rng2.gen_range(0..N_NEURONS);
 
+            /* build parameters of the network */
             let input_spikes: Vec<Vec<Vec<u8>>> = read_multiple_input_spikes(&path);
-
             let neurons: Vec<LifNeuron> = build_neurons(&path);
             let extra_weights: Vec<Vec<f64>> = read_extra_weights(&path);
             let intra_weights: Vec<Vec<f64>> = build_intra_weights();
 
-            let vec_type_fail = vec![Failure::StuckAt1(StuckAt1::new(random_bit)),Failure::StuckAt0(StuckAt0::new(random_bit)), Failure::TransientBitFlip(TransientBitFlip::new(random_bit))];
+            let vec_type_fail = vec![
+                Failure::StuckAt1(StuckAt1::new(random_bit)),
+                Failure::StuckAt0(StuckAt0::new(random_bit)),
+                Failure::TransientBitFlip(TransientBitFlip::new(random_bit))];
 
             //fail 0 piero, fail 1 vitto, fail 2 giorgio
-            let configuration = Conf::new(vec![elem.clone()], vec_type_fail[0].clone(), random_index);
+            let configuration = Conf::new(vec![elem.clone()], vec_type_fail[1].clone(), random_index);
 
             let file_name = get_file_name(&configuration);
             let path_output = format!("{path}/simulation/configurations/{file_name}");
             let mut output_file = File::create(path_output).expect("Something went wrong opening the file outputCounters.txt!");
 
-            for i in 0..CYCLES{
-
+            /* run simulation over snn with fault configuration */
+            for i in 0..CYCLES {
                 let mut snn = SnnBuilder::new(N_INPUTS)
-                    .add_layer(neurons.clone(), extra_weights.clone(), intra_weights.clone(),configuration.clone())
+                    .add_layer(neurons.clone(), extra_weights.clone(), intra_weights.clone(), configuration.clone())
                     .build();
 
                 println!("Iteration {i}");
                 let output_spikes = snn.process(&input_spikes[i]);
 
-                let mut neurons_sum = vec![0u32;400];
+                let mut neurons_sum = vec![0u32; 400];
 
                 for k in 0..N_NEURONS {
                     for j in 0..N_INSTANTS {
@@ -112,86 +86,80 @@ fn start_snn(){
                 }
             }
         }
-    }*/
+    }
+
+    /* when all simulations are finished run the python script to print logs file */
     let string = format!("{path}/simulation/runSimulation.py");
     let path_py = OsStr::new(&string);
-    let output = Command::new("python").arg(path_py).output()
-    .expect("Errore durante l'esecuzione del comando");
-
-    println!("Output del comando Python: {:?}", output);
+    Command::new("python").arg(path_py).output()
+        .expect("Errore durante l'esecuzione del comando");
 }
 
-fn get_file_name(conf:&Conf)->String{
+fn get_file_name(conf: &Conf) -> String {
     let components = conf.get_vec_components();
     let comp = components[0].clone();
 
     let comp_string = match comp {
-        Components::VTh => {"VTh"}
-        Components::VRest => {"VRest"}
-        Components::VReset => {"VReset"}
-        Components::Tau => {"Tau"}
-        Components::VMem => {"VMem"}
-        Components::Ts => {"Ts"}
-        Components::Dt => {"Dt"}
-        Components::Weights => {"Weights"}
-        Components::IntraWeights => {"IntraWeights"}
-        Components::PrevSpikes => {"PrevSpikes"}
-        Components::None => {"None"}
+        Components::VTh => { "VTh" }
+        Components::VRest => { "VRest" }
+        Components::VReset => { "VReset" }
+        Components::Tau => { "Tau" }
+        Components::VMem => { "VMem" }
+        Components::Ts => { "Ts" }
+        Components::Dt => { "Dt" }
+        Components::Weights => { "Weights" }
+        Components::IntraWeights => { "IntraWeights" }
+        Components::PrevSpikes => { "PrevSpikes" }
+        Components::None => { "None" }
     };
 
     let failure = conf.get_failure();
 
     let failure_string =
         match failure {
-            Failure::StuckAt0(s) => {format!("StuckAt0_{}",s.get_position())}
-            Failure::StuckAt1(s) => {format!("StuckAt1_{}",s.get_position())}
-            Failure::TransientBitFlip(t) => {format!("Transient_{}",t.get_position())}
-            Failure::None => {"None".to_string()}
+            Failure::StuckAt0(s) => { format!("StuckAt0_{}", s.get_position()) }
+            Failure::StuckAt1(s) => { format!("StuckAt1_{}", s.get_position()) }
+            Failure::TransientBitFlip(t) => { format!("Transient_{}", t.get_position()) }
+            Failure::None => { "None".to_string() }
         };
     let neuron_index = conf.get_index_neuron();
 
     format!("{comp_string}_{failure_string}_{neuron_index}.txt")
-
 }
 
-fn read_multiple_input_spikes(path: &String)->Vec<Vec<Vec<u8>>>{
-
+fn read_multiple_input_spikes(path: &String) -> Vec<Vec<Vec<u8>>> {
     let path_input = format!("{path}/simulation/inputSpikes.txt");
     let input = File::open(path_input).expect("Something went wrong opening the file inputSpikes.txt!");
     let buffered = BufReader::new(input);
 
-    let mut vec_input_spikes:Vec<Vec<Vec<u8>>> = vec![vec![vec![0; N_INSTANTS]; N_INPUTS];CYCLES];
+    let mut vec_input_spikes: Vec<Vec<Vec<u8>>> = vec![vec![vec![0; N_INSTANTS]; N_INPUTS]; CYCLES];
 
     let mut i = 0;
     let mut count = 0;
     let mut input_spikes: Vec<Vec<u8>> = vec![vec![0; N_INSTANTS]; N_INPUTS];
 
     for line in buffered.lines() {
-
-        let l = line.unwrap().replace("\n","");
+        let c = '\n';
+        let l = line.unwrap().replace(c, "");
         let mut j = 0;
         let chars = convert_line_into_u8(l);
-        chars.into_iter().for_each(| ch | {
+        chars.into_iter().for_each(|ch| {
             input_spikes[j][i] = ch;
             j += 1;
         });
 
         i += 1;
 
-        if i%N_INSTANTS==0 && i>0{
-            i=0;
+        if i % N_INSTANTS == 0 && i > 0 {
+            i = 0;
             vec_input_spikes[count] = input_spikes.clone();
-            count+=1;
+            count += 1;
             if count == CYCLES {
                 break;
             }
         }
-
     }
-
     vec_input_spikes
-
-
 }
 
 /* * USEFUL FUNCTIONS * */
@@ -199,13 +167,13 @@ fn read_multiple_input_spikes(path: &String)->Vec<Vec<Vec<u8>>>{
 /**
 This function builds the neurons of the network.
  */
-fn build_neurons(path:&String) -> Vec<LifNeuron> {
+fn build_neurons(path: &String) -> Vec<LifNeuron> {
     let thresholds: Vec<f64> = read_thresholds(path);
 
     let v_rest: f64 = -65.0;
     let v_reset: f64 = -60.0;
     let tau: f64 = 100.0;
-    let dt: f64 = 0.1; 
+    let dt: f64 = 0.1;
 
     let mut neurons: Vec<LifNeuron> = Vec::with_capacity(N_NEURONS);
 
@@ -243,7 +211,7 @@ fn build_intra_weights() -> Vec<Vec<f64>> {
 /*
 This function reads the weights file and returns a 2D Vec of weights
  */
-fn read_extra_weights(path:&String) -> Vec<Vec<f64>> {
+fn read_extra_weights(path: &String) -> Vec<Vec<f64>> {
     let path_weights_file = format!("{path}/simulation/networkParameters/weightsOut.txt");
 
     let input = File::open(path_weights_file).expect("Something went wrong opening the file weightsOut.txt!");
@@ -270,8 +238,7 @@ fn read_extra_weights(path:&String) -> Vec<Vec<f64>> {
 This function reads the threshold file and returns a Vec of thresholds
  */
 
-fn read_thresholds(path:&String) -> Vec<f64> {
-
+fn read_thresholds(path: &String) -> Vec<f64> {
     let path_threshold_file = format!("{path}/simulation/networkParameters/thresholdsOut.txt");
 
     let input = File::open(path_threshold_file).expect("Something went wrong opening the file thresholdsOut.txt!");
@@ -279,13 +246,8 @@ fn read_thresholds(path:&String) -> Vec<f64> {
 
     let mut thresholds: Vec<f64> = vec![0f64; N_NEURONS];
 
-    let mut i = 0;
-
-    for line in buffered.lines() {
-
+    for (i,line) in buffered.lines().enumerate() {
         thresholds[i] = line.unwrap().parse::<f64>().expect("Cannot parse String into f64!");
-
-        i += 1;
     }
 
     thresholds
@@ -298,4 +260,32 @@ fn convert_line_into_u8(line: String) -> Vec<u8> {
     line.chars()
         .map(|ch| (ch.to_digit(10).unwrap()) as u8)
         .collect::<Vec<u8>>()
+}
+
+
+fn get_current_dir() -> String {
+    let current_dir = std::env::current_dir().expect("Failed to get current directory");
+
+    // Convert the PathBuf to a string
+    let current_dir_str = current_dir.to_string_lossy();
+
+    // Replace backslashes with forward slashes
+    let c = '\\';
+    current_dir_str.replace(c, "/")
+}
+
+fn read_zip(path: &String) -> std::io::Result<()> {
+    let extract_to_dir = format!("{path}/simulation/");
+    let zip_file_path = format!("{extract_to_dir}inputSpikes.zip");
+
+    let file = File::open(zip_file_path)?;
+    let mut archive = ZipArchive::new(file)?;
+
+    let mut file = archive.by_index(0)?;
+    let output_path = format!("{extract_to_dir}inputSpikes.txt");
+
+    let mut outfile = File::create(output_path)?;
+    std::io::copy(&mut file, &mut outfile)?;
+
+    Ok(())
 }
